92514af7db174a0551ea70ca0770c653cebb8af1
lemon78787878 <wakuwaku.5611@gmail.com>
2024-07-23 00:53:36 +0900

    ルーティングの整理

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
     root to: 'devise/sessions#new'
     get 'user', to: 'users#show', as: :user_root
   end
+
   resources :foods, only: [:index, :new, :create, :destroy, :edit, :update]
   resources :recipes, only: [:new, :create, :show]
-  resources :diaries, except: :show
-  get 'diaries/date/:date', to: 'diaries#date', as: 'diaries_by_date'
-  get 'diaries/edit/:date', to: 'diaries#edit_by_date', as: 'edit_diaries_by_date'
-  patch 'diaries/update_by_date/:date', to: 'diaries#update_by_date', as: 'update_diaries_by_date'
 
-
+  resources :diaries, except: :show do
+    collection do
+      get 'date/:date', to: 'diaries#date', as: 'by_date'
+      get 'edit/:date', to: 'diaries#edit_by_date', as: 'edit_by_date'
+      patch 'update_by_date/:date', to: 'diaries#update_by_date', as: 'update_by_date'
+    end
+  end
 end
